5fdb7de7f70a9854cc2003d3db89e101d7355cd8
anguswbeef <angelitafu.19@gmail.com>
2026-02-14 02:33:25 +0700

    Create animate2.js

---
+++ animate2.js
@@ -0,0 +1,13 @@
+const images = [
+"assets/bread (1).svg",
+"assets/bread (2).svg",
+"assets/bread (3).svg"
+];
+
+let index = 0;
+const character = document.getElementById("object1");
+
+setInterval(() => {
+index = (index + 1) % images.length;
+character.src = images[index];
+}, 500);
