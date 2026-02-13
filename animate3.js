4f31ae6edfb0dcb1a6884354552ef6fdf782a52e
anguswbeef <angelitafu.19@gmail.com>
2026-02-14 02:33:50 +0700

    Create animate3.js

---
+++ animate3.js
@@ -0,0 +1,24 @@
+const images1 = [
+  "assets/bread (1).svg",
+  "assets/bread (2).svg",
+  "assets/bread (4).svg"
+];
+
+const images2 = [
+  "assets/gus n kris (1).svg",
+  "assets/gus n kris (2).svg",
+  "assets/gus n kris (3).svg"
+];
+
+let index1 = 0;
+let index2 = 0;
+const character1 = document.getElementById("object2");
+const character2 = document.getElementById("object3");
+
+setInterval(() => {
+  index1 = (index1 + 1) % images1.length;
+  index2 = (index2 + 1) % images2.length;
+  
+  character1.src = images1[index1];
+  character2.src = images2[index2];
+}, 500);
